8fa6de4cc2f0ebf77d1608852624748a1ba6e2b3
1122c <118850768+1122c@users.noreply.github.com>
2024-03-03 11:45:41 -0800

    edited file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // import { useState } from 'react'
-import Header from './components/Header'
-import About from './components/About'
-import Portfolio from './components/Portfolio'
-import Contact from './components/Contact'
-import Resume from './components/Resume'
+import Header from './components/Header/Header'
+import About from './components/pages/About'
+import Portfolio from './components/pages/Portfolio'
+import Contact from './components/pages/Contact'
+import Resume from './components/pages/Resume'
 import './App.css'
 
 function App() {
