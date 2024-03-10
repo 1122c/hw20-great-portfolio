f6dac9d0c8d1785d19216df59f2c14acc13c0e20
1122c <118850768+1122c@users.noreply.github.com>
2024-03-10 11:20:39 -0700

    changes to about file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,13 @@
-
-
-function About(){
-
-   
-
-    return(
+function About() {
+    return (
         <div className="main">
-            <h2>This is the about page</h2>
+            <h2>ABOUT ME</h2>
             <h2 className="headertest">I am a developer</h2>
             
-            {/* insert about info here */}
+            <p>Blending my background in healthcare and software engineering, I've developed a nuanced approach to solving complex problems with precision and creativity. As a physical therapist assistant, I cultivated a keen eye for detail and patient-centered solutions, now informing my software development. My involvement in a genomic project enriched my expertise, particularly in data visualization, enabling me to effectively present complex data. I am eager to combine my unique mix of healthcare insights, software engineering skills, and data visualization experience from this project to create impactful web applications.</p>
         </div>
-    )
+    );
 }
 
 export default About;
+
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,21 +5,25 @@ function Resume(){
 
     return(
         <div className="main">
-            <h2>This is the Resume page</h2>
+            <h2>Resume page</h2>
             {/* list front end and backend proficiences */}
-        <h2>Check out my <a href={resume} download>resume</a></h2>
+        <h2>view <a href={resume} download>resume</a></h2>
             <h2>Front End Proficiences</h2>
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
-                <li>etc..</li>
+                <li>JavaScript</li>
+                <li>React</li>
             </ul>
 
             <h2>Backend End Proficiences</h2>
             <ul>
                 <li>Node.js</li>
                 <li>Express.js</li>
-                <li>etc..</li>
+                <li>mySQL</li>
+                <li>noSQL</li>
+                <li>MongoDB</li>
+                <li>Python</li>
             </ul>
         </div>
     )
